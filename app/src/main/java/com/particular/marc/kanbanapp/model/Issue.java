7a0b2b5df72ffe93effa4681b82dfbdb57234ed4
MrMPN <portabella.marc@gmail.com>
2018-10-21 15:19:25 +0200

    Annotated Issue and Repo as Entities + added RepoAndAllIssues class.

--- app/src/main/java/com/particular/marc/kanbanapp/model/Issue.java
+++ app/src/main/java/com/particular/marc/kanbanapp/model/Issue.java
@@ -1,4 +1,11 @@
 package com.particular.marc.kanbanapp.model;
 
+import android.arch.persistence.room.Entity;
+
+/**
+ * A class that represents the issues of a given repository.
+ * Both Room and Retrofit will use it
+ */
+@Entity
 public class Issue {
 }
--- app/src/main/java/com/particular/marc/kanbanapp/model/Repo.java
+++ app/src/main/java/com/particular/marc/kanbanapp/model/Repo.java
@@ -1,4 +1,11 @@
 package com.particular.marc.kanbanapp.model;
 
+import android.arch.persistence.room.Entity;
+
+/**
+ * Class that represents a repository.
+ * Both Room and Retrofit will use it
+ */
+@Entity
 public class Repo {
 }

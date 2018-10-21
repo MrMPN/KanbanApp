4422cb40deaee488841758133cca544869f87131
MrMPN <portabella.marc@gmail.com>
2018-10-21 18:37:52 +0200

    Implementation of Repo and Issue classes

--- app/src/main/java/com/particular/marc/kanbanapp/model/Issue.java
+++ app/src/main/java/com/particular/marc/kanbanapp/model/Issue.java
@@ -1,6 +1,9 @@
 package com.particular.marc.kanbanapp.model;
 
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import com.google.gson.annotations.Expose;
 
 /**
  * A class that represents the issues of a given repository.
@@ -8,4 +11,58 @@ import android.arch.persistence.room.Entity;
  */
 @Entity(tableName = "issue")
 public class Issue {
+    @PrimaryKey
+    private int id;
+
+    private String title;
+    private String body;
+
+    @Expose(serialize = false, deserialize = false)
+    private int repo_id;
+
+    @Expose(serialize = false, deserialize = false)
+    private int board = 0;
+
+    public Issue() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getRepo_id() {
+        return repo_id;
+    }
+
+    public void setRepo_id(int repo_id) {
+        this.repo_id = repo_id;
+    }
+
+    public int getBoard() {
+        return board;
+    }
+
+    public void setBoard(int board) {
+        this.board = board;
+    }
 }
--- app/src/main/java/com/particular/marc/kanbanapp/model/Repo.java
+++ app/src/main/java/com/particular/marc/kanbanapp/model/Repo.java
@@ -1,6 +1,12 @@
 package com.particular.marc.kanbanapp.model;
 
+import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Class that represents a repository.
@@ -8,4 +14,60 @@ import android.arch.persistence.room.Entity;
  */
 @Entity(tableName = "repo")
 public class Repo {
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo(name = "full_name")
+    @SerializedName("full_name")
+    private String fullName;
+
+    @ColumnInfo(name = "issues_count")
+    @SerializedName("open_issues_count")
+    private int issuesCount;
+
+    @ColumnInfo(name = "has_kanban")
+    @Expose(serialize = false, deserialize = false)
+    private boolean hasKanban = false;
+
+    @Ignore
+    public Repo(int id, String fullName, int issuesCount) {
+        this.id = id;
+        this.fullName = fullName;
+        this.issuesCount = issuesCount;
+    }
+
+    public Repo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getIssuesCount() {
+        return issuesCount;
+    }
+
+    public void setIssuesCount(int issuesCount) {
+        this.issuesCount = issuesCount;
+    }
+
+    public boolean isHasKanban() {
+        return hasKanban;
+    }
+
+    public void setHasKanban(boolean hasKanban) {
+        this.hasKanban = hasKanban;
+    }
 }

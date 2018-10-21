778aac9480390661e6506bbf696d5082bce54e52
MrMPN <portabella.marc@gmail.com>
2018-10-21 16:53:33 +0200

    Added repository

---
+++ app/src/main/java/com/particular/marc/kanbanapp/repository/RepoRepository.java
@@ -0,0 +1,9 @@
+package com.particular.marc.kanbanapp.repository;
+
+/**
+ * Repository that will handle all the data-fetching related tasks, be it from the server or local.
+ * It uses the Room Database as DataSource to pass into the viewmodel, but it'll use the server
+ * to fetch more data when running out
+ */
+public class RepoRepository {
+}

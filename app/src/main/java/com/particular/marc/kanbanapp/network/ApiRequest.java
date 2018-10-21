32aa99349f2b5c88a0b68047dfd5cc8e5461bada
MrMPN <portabella.marc@gmail.com>
2018-10-21 18:10:13 +0200

    Implementation of ApiRequest

--- app/src/main/java/com/particular/marc/kanbanapp/network/ApiRequest.java
+++ app/src/main/java/com/particular/marc/kanbanapp/network/ApiRequest.java
@@ -1,9 +1,30 @@
 package com.particular.marc.kanbanapp.network;
 
+import com.particular.marc.kanbanapp.model.Issue;
+import com.particular.marc.kanbanapp.model.Repo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 /**
  * Interface that defines the methods to query the GitHub API.
  * Retrofit will implement it
  */
 public interface ApiRequest {
+    @GET("users/JakeWharton/repos")
+    Call<List<Repo>> getAllRepos(
+            @Query("page") int page,
+            @Query("per_page") int perPage
+            );
 
+    @GET("/repos/JakeWharton/{repoName}/issues")
+    Call<List<Issue>> getAllIssuesByRepo(
+            @Path("repoName") String name,
+            @Query("page") int page,
+            @Query("per_page") int perPage
+    );
 }

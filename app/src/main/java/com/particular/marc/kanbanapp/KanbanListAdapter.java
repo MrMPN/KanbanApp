70dc3cd0b722b5e78a3f8d5e9b868e80cec6ef4b
MrMPN <portabella.marc@gmail.com>
2018-10-21 17:57:52 +0200

    design-finished

--- app/src/main/java/com/particular/marc/kanbanapp/KanbanListAdapter.java
+++ app/src/main/java/com/particular/marc/kanbanapp/KanbanListAdapter.java
@@ -4,5 +4,8 @@ import android.arch.paging.PagedListAdapter;
 
 import com.particular.marc.kanbanapp.model.Issue;
 
+/**
+ * PagedListAdapter that will take a PagedList and display it into the RecyclerView
+ */
 public class KanbanListAdapter extends PagedListAdapter<Issue, KanbanListAdapter.ViewHolder> {
 }
--- app/src/main/java/com/particular/marc/kanbanapp/RepoListAdapter.java
+++ app/src/main/java/com/particular/marc/kanbanapp/RepoListAdapter.java
@@ -4,5 +4,8 @@ import android.arch.paging.PagedListAdapter;
 
 import com.particular.marc.kanbanapp.model.Repo;
 
+/**
+ * PagedListAdapter that will take a PagedList and display it into the RecyclerView
+ */
 public class RepoListAdapter extends PagedListAdapter<Repo, RepoListAdapter.ViewHolder> {
 }

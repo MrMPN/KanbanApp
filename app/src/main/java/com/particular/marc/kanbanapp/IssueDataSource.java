7b051181f4cd6d3229c9b9f1eaf7b1fd0e92790a
MrMPN <portabella.marc@gmail.com>
2018-10-21 17:33:43 +0200

    Added 2 PageKeyedDataSource subclasses

---
+++ app/src/main/java/com/particular/marc/kanbanapp/IssueDataSource.java
@@ -0,0 +1,8 @@
+package com.particular.marc.kanbanapp;
+
+import android.arch.paging.PageKeyedDataSource;
+
+import com.particular.marc.kanbanapp.model.Issue;
+
+public class IssueDataSource extends PageKeyedDataSource<Integer, Issue> {
+}
---
+++ app/src/main/java/com/particular/marc/kanbanapp/RepoDataSource.java
@@ -0,0 +1,8 @@
+package com.particular.marc.kanbanapp;
+
+import android.arch.paging.PageKeyedDataSource;
+
+import com.particular.marc.kanbanapp.model.Repo;
+
+public class RepoDataSource extends PageKeyedDataSource<Integer, Repo> {
+}

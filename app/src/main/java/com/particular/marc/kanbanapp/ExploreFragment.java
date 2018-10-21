95f113c6c05ad7f6ce93d4f10aa8d0ae1a0398c1
MrMPN <portabella.marc@gmail.com>
2018-10-21 13:36:48 +0200

    Added 2 fragments for the MainActivity

---
+++ app/src/main/java/com/particular/marc/kanbanapp/ExploreFragment.java
@@ -0,0 +1,31 @@
+package com.particular.marc.kanbanapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * This will be the initial fragment the user will see. It displays a list of repositories of a
+ * given user
+ */
+public class ExploreFragment extends Fragment {
+
+
+    public ExploreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_explore, container, false);
+    }
+
+}

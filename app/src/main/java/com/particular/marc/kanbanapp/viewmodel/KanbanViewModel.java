d173a07874275cceda62dd97043a5c16d44720b8
MrMPN <portabella.marc@gmail.com>
2018-10-21 20:06:11 +0200

    Implementation of the ViewModels

--- app/src/main/java/com/particular/marc/kanbanapp/viewmodel/KanbanViewModel.java
+++ app/src/main/java/com/particular/marc/kanbanapp/viewmodel/KanbanViewModel.java
@@ -1,6 +1,13 @@
 package com.particular.marc.kanbanapp.viewmodel;
 
+import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.paging.PagedList;
+import android.support.annotation.NonNull;
+
+import com.particular.marc.kanbanapp.model.RepoAndAllissues;
+import com.particular.marc.kanbanapp.repository.RepoRepository;
 
 /**
  * ViewModel that will hold all the issues data (list of Issue)
@@ -8,4 +15,10 @@ import android.arch.lifecycle.AndroidViewModel;
  * (Backlog,Next,Doing,Done) fragments will observe methods from this
  */
 public class KanbanViewModel extends AndroidViewModel {
+    private RepoRepository repository;
+    private LiveData<PagedList<RepoAndAllissues>> issues;
+
+    public KanbanViewModel(@NonNull Application application) {
+        super(application);
+    }
 }
--- app/src/main/java/com/particular/marc/kanbanapp/viewmodel/MainViewModel.java
+++ app/src/main/java/com/particular/marc/kanbanapp/viewmodel/MainViewModel.java
@@ -1,6 +1,14 @@
 package com.particular.marc.kanbanapp.viewmodel;
 
+import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.paging.PagedList;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.particular.marc.kanbanapp.model.Repo;
+import com.particular.marc.kanbanapp.repository.RepoRepository;
 
 /**
  * ViewModel that will hold all the repos data (list of Repo)
@@ -8,4 +16,18 @@ import android.arch.lifecycle.AndroidViewModel;
  * Both Explore and Local fragments will observe methods from this
  */
 public class MainViewModel extends AndroidViewModel {
+    private static final String TAG = "MainViewModel";
+    private RepoRepository repository;
+    private LiveData<PagedList<Repo>> repos;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        repository = RepoRepository.getInstance(application);
+        repos = repository.getReposList();
+    }
+
+    public LiveData<PagedList<Repo>> getRepos() {
+        Log.d(TAG, "MainViewModel:" + repos.getValue());
+        return repos;
+    }
 }

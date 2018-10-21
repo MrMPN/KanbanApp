6f367fc6394ec5abbf7491f7deac863f7b1c3b75
MrMPN <portabella.marc@gmail.com>
2018-10-21 16:50:01 +0200

    Added network package (ApiRequest interface and RetrofitClient class)

---
+++ app/src/main/java/com/particular/marc/kanbanapp/network/ApiRequest.java
@@ -0,0 +1,9 @@
+package com.particular.marc.kanbanapp.network;
+
+/**
+ * Interface that defines the methods to query the GitHub API.
+ * Retrofit will implement it
+ */
+public interface ApiRequest {
+
+}
---
+++ app/src/main/java/com/particular/marc/kanbanapp/network/RetrofitClient.java
@@ -0,0 +1,9 @@
+package com.particular.marc.kanbanapp.network;
+
+/**
+ *Class to build and hold a Retrofit instance.
+ * Will interact with ApiRequest class to fetch data from GitHub API
+ */
+public class RetrofitClient {
+
+}

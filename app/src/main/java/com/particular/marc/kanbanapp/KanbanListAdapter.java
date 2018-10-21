e51ee26920af4997d86eb522364fbacf0f770d2b
MrMPN <portabella.marc@gmail.com>
2018-10-21 17:29:10 +0200

    Added 2 PagedListAdapter subclasses

---
+++ app/src/main/java/com/particular/marc/kanbanapp/KanbanListAdapter.java
@@ -0,0 +1,8 @@
+package com.particular.marc.kanbanapp;
+
+import android.arch.paging.PagedListAdapter;
+
+import com.particular.marc.kanbanapp.model.Issue;
+
+public class KanbanListAdapter extends PagedListAdapter<Issue, KanbanListAdapter.ViewHolder> {
+}
---
+++ app/src/main/java/com/particular/marc/kanbanapp/RepoListAdapter.java
@@ -0,0 +1,8 @@
+package com.particular.marc.kanbanapp;
+
+import android.arch.paging.PagedListAdapter;
+
+import com.particular.marc.kanbanapp.model.Repo;
+
+public class RepoListAdapter extends PagedListAdapter<Repo, RepoListAdapter.ViewHolder> {
+}

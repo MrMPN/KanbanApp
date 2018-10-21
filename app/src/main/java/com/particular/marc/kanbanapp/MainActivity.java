f88c49b5efa6de314a1364cce006449294ff1d65
MrMPN <portabella.marc@gmail.com>
2018-10-21 13:53:34 +0200

    Added PagerAdapter subclass implementation

--- app/src/main/java/com/particular/marc/kanbanapp/MainActivity.java
+++ app/src/main/java/com/particular/marc/kanbanapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.particular.marc.kanbanapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setViewPager();
+    }
+
+    private void setViewPager(){
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = findViewById(R.id.main_viewpager);
+        // Create an adapter that knows which fragment should be shown on each page
+        MainActivityPagerAdapter adapter = new MainActivityPagerAdapter(getSupportFragmentManager(),
+                this);
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = findViewById(R.id.main_sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/com/particular/marc/kanbanapp/MainActivityPagerAdapter.java
@@ -0,0 +1,54 @@
+package com.particular.marc.kanbanapp;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * FragmentPagerAdapter subclass used to populate the ViewPager, so the user can swipe between the
+ * Local and Explore fragments
+ */
+public class MainActivityPagerAdapter extends FragmentPagerAdapter {
+    private final int PAGE_COUNT = 2;
+    Context context;
+
+    public MainActivityPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch (i){
+            case 0:
+                return new ExploreFragment();
+            case 1:
+                return new LocalFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+
+            case 0:
+                return context.getString(R.string.explore);
+
+            case 1:
+                return context.getString(R.string.local);
+
+            default:
+                return null;
+        }
+    }
+}

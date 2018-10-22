96999444e9a71d1c5645c2ab97f1ee6e361f6419
MrMPN <portabella.marc@gmail.com>
2018-10-22 20:32:08 +0200

    KanbanActivity and its fragment share the viewModel now, in order to pass the repoId

--- app/src/main/java/com/particular/marc/kanbanapp/KanbanActivity.java
+++ app/src/main/java/com/particular/marc/kanbanapp/KanbanActivity.java
@@ -1,19 +1,40 @@
 package com.particular.marc.kanbanapp;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.particular.marc.kanbanapp.adapter.KanbanPagerAdapter;
+import com.particular.marc.kanbanapp.fragments.LocalFragment;
+import com.particular.marc.kanbanapp.viewmodel.KanbanViewModel;
 
 public class KanbanActivity extends AppCompatActivity {
+    private static final String TAG = "KanbanActivity";
+    private ViewModel viewModel;
+    private int repoId;
+    private String repoName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kanban);
+        getRepoFromIntent();
+        setTitle(repoName);
+        viewModel = ViewModelProviders.of(this).get(KanbanViewModel.class);
+        ((KanbanViewModel) viewModel).getIssuesByRepo(repoId);
         setViewPager();
+    }
+
+    private void getRepoFromIntent(){
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("repoId")){
+            repoId = intent.getIntExtra(LocalFragment.REPO_ID, 0);
+            repoName = intent.getStringExtra(LocalFragment.REPO_NAME);
+        }
     }
 
     private void setViewPager(){
--- app/src/main/java/com/particular/marc/kanbanapp/fragments/KanbanBoardFragment.java
+++ app/src/main/java/com/particular/marc/kanbanapp/fragments/KanbanBoardFragment.java
@@ -1,13 +1,23 @@
 package com.particular.marc.kanbanapp.fragments;
 
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.particular.marc.kanbanapp.R;
+import com.particular.marc.kanbanapp.adapter.KanbanListAdapter;
+import com.particular.marc.kanbanapp.model.Issue;
+import com.particular.marc.kanbanapp.viewmodel.KanbanViewModel;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,18 +25,49 @@ import com.particular.marc.kanbanapp.R;
  * Displays a recyclerView with the list of backlog issues (default state when newly created)
  */
 public class KanbanBoardFragment extends Fragment {
-
+    private static final String TAG = "KanbanBoardFragment";
+    private KanbanViewModel viewModel;
+    private KanbanListAdapter adapter;
+    private int board;
 
     public KanbanBoardFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_base, container, false);
+        View v = inflater.inflate(R.layout.fragment_base, container, false);
+        getBundleData();
+        setRecyclerView(v);
+        initViewModel();
+        return v;
     }
 
+    /**
+     * This method will provide us with the int that tells us the board we are
+     */
+    private void getBundleData(){
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            board = bundle.getInt("board", 0);
+        }
+    }
+
+    private void setRecyclerView(View v){
+        RecyclerView recyclerView = v.findViewById(R.id.list);
+        adapter = new KanbanListAdapter(getContext(), board);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    private void initViewModel(){
+        viewModel = ViewModelProviders.of(getActivity()).get(KanbanViewModel.class);
+        viewModel.getIssuesByRepoFilteredByBoard(board).observe(getViewLifecycleOwner(), new Observer<List<Issue>>() {
+            @Override
+            public void onChanged(@Nullable List<Issue> issues) {
+                adapter.submitList(issues);
+            }
+        });
+    }
 }

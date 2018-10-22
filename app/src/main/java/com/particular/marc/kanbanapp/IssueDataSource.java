d68b85dd5519b464d0533c3bd40ed489b6055491
MrMPN <portabella.marc@gmail.com>
2018-10-22 18:57:39 +0200

    Deleted DataSources classes, not needed.

--- app/src/main/java/com/particular/marc/kanbanapp/IssueDataSource.java
+++
@@ -1,8 +0,0 @@
-package com.particular.marc.kanbanapp;
-
-import android.arch.paging.PageKeyedDataSource;
-
-import com.particular.marc.kanbanapp.model.Issue;
-
-public class IssueDataSource extends PageKeyedDataSource<Integer, Issue> {
-}
--- app/src/main/java/com/particular/marc/kanbanapp/RepoDataSource.java
+++
@@ -1,8 +0,0 @@
-package com.particular.marc.kanbanapp;
-
-import android.arch.paging.PageKeyedDataSource;
-
-import com.particular.marc.kanbanapp.model.Repo;
-
-public class RepoDataSource extends PageKeyedDataSource<Integer, Repo> {
-}

2d3e676e6fa4a7493f925e71a7cb9474005b3ecf
MrMPN <portabella.marc@gmail.com>
2018-10-22 20:20:12 +0200

    Methods for KanbanViewModel implemented, Java changed to version 8

--- app/src/main/java/com/particular/marc/kanbanapp/viewmodel/KanbanViewModel.java
+++ app/src/main/java/com/particular/marc/kanbanapp/viewmodel/KanbanViewModel.java
@@ -3,17 +3,19 @@ package com.particular.marc.kanbanapp.viewmodel;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Transformations;
 import android.support.annotation.NonNull;
 
 import com.particular.marc.kanbanapp.model.Issue;
 import com.particular.marc.kanbanapp.repository.RepoRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * ViewModel that will hold all the issues data (list of Issue)
  * It gets its LiveData from the RepoRepository class
- * (Backlog,Next,Doing,Done) fragments will observe methods from this
+ * KanbanBoardfragment will observe methods from this
  */
 public class KanbanViewModel extends AndroidViewModel {
     private static final String TAG = "KanbanViewModel";
@@ -24,8 +26,28 @@ public class KanbanViewModel extends AndroidViewModel {
     private RepoRepository repository;
     private LiveData<List<Issue>> issues;
 
-
     public KanbanViewModel(@NonNull Application application) {
         super(application);
+        repository = RepoRepository.getInstance(application);
+    }
+
+    public void getIssuesByRepo(int repoId){
+        issues =repository.getIssuesByRepo(repoId);
+    }
+
+    public LiveData<List<Issue>> getIssuesByRepoFilteredByBoard(int board){
+        return Transformations.map(issues, list -> {
+            List<Issue> filteredList = new ArrayList<>();
+            for (Issue issue: list){
+                if (issue.getBoard() == board){
+                    filteredList.add(issue);
+                }
+            }
+            return filteredList;
+        });
+    }
+
+    public void updateIssue(Issue issue){
+        repository.updateIssue(issue);
     }
 }
